d57bd04a83f3afc1a83048e3bec301bbd95faf71
Mahdi-Falahti <falahatimahdi01@gmail.com>
2021-04-01 20:06:17 +0430

    fix bug(delete comma in number) and write method reduce Money (for validate money sprnd)

--- js/class.js
+++ js/class.js
@@ -18,10 +18,12 @@ class GetInformations {
         } else {
             price = inputPrice.value;
             name = inputName.value;
-            this.showSpendItem(name, price);
             inputPrice.style.boxShadow = "none";
-            inputName.value = "";
+            this.showSpendItem(name, price);
+            this.reduceMoney(price);
             inputPrice.value = "";
+            inputName.value = "";
+
         }
 
     }
@@ -42,7 +44,7 @@ class GetInformations {
 
         //crete element li for show spend item
         const li = document.createElement("li");
-        li.innerHTML = "هزینه  " + name + "  " + money + " هزار تومان"
+        li.innerHTML = `هزینه  ${name}  <p class="lists"> ${money} </P> تومان `;
 
         const ul = document.querySelector(".spending");
         ul.appendChild(li);
@@ -64,6 +66,7 @@ class GetInformations {
         //create span for show budget
         let span = document.createElement("span");
         span.innerHTML = money;
+        span.classList = "span";
 
         // budget
         let totalMoney = document.querySelector(".totoal-money");
@@ -73,10 +76,13 @@ class GetInformations {
         //create span for show left Over Money
         let spanLOM = document.createElement("span");
 
+        spanLOM.classList = "spanLOM"
 
         spanLOM.innerHTML = money;
 
         leftOverMoney.firstElementChild.appendChild(spanLOM);
+
+
 
     }
 
@@ -86,5 +92,50 @@ class GetInformations {
         let moneySeprated = this.addComma(moneySpend);
         inputPrice.value = moneySeprated;
         return moneySpend
+    }
+
+    reduceMoney(Price) {
+        // selctors
+        const span = document.querySelector(".span"),
+            price = Number(Price.replace(/,/g, "")),
+            spanLOM = document.querySelector(".spanLOM");
+        let list = document.querySelectorAll(".lists");
+
+        // check input price when user write price more than budget write a text 
+        if (price > span.innerHTML.replace(/,/g, "")) {
+
+            let count = 0;
+            for (let i = 0; i < list.length; i++) {
+                if (Number(list[i].innerHTML.replace(/,/g, "")) > price) {
+                    count = i;
+                }
+            }
+
+            // add class eror to li for add style
+            list[count].parentElement.classList += " eror";
+            list[count].parentElement.innerHTML = " چی !!! نه خدایی یکم فکر کن تو اصلا انقدر بودجه داری";
+
+            // check input price when user write price negative or Enter price but - width left over money if money is negative  his/her budget write a text 
+        } else if (Number(spanLOM.innerHTML.replace(/,/g, "") - price) < 0) {
+
+            let count = 0;
+            for (let i = 0; i < list.length; i++) {
+
+                if (list[i].innerHTML.replace(/,/g, "") == price) {
+                    count = i;
+                }
+            }
+
+            // add class eror to li for add style
+            list[count].parentElement.classList += " eror";
+            list[count].parentElement.innerHTML = "دیدی چی شد پولت نمیکشه : (";
+
+        } else {
+
+            // calculate result
+            let p = spanLOM.innerHTML.replace(/,/g, "") - price;
+            spanLOM.innerHTML = p;
+
+        }
     }
 }
