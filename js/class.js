4d8c8a1e2f297365594b5dad64f7925460c03fd1
Mahdi-Falahti <falahatimahdi01@gmail.com>
2021-04-02 13:51:35 +0430

    load content of local storage

--- js/class.js
+++ js/class.js
@@ -50,50 +50,99 @@ class GetInformations {
         ul.appendChild(li);
 
         // add price to lacal storage 
-        let prices = this.checkContentExistINlocalStorage("prices");
-        prices.push(li.innerHTML);
-        localStorage.setItem("prices", JSON.stringify(prices));
+        let factors = this.checkContentExistINlocalStorage("factors");
+        factors.push(li.innerHTML);
+        localStorage.setItem("factors", JSON.stringify(factors));
     }
 
 
 
-    geBudgetAndShow() {
-        // get all the money OF user 
-        let budget = prompt("لطفا مقدار بودجه ی خود را به تومان وارد کنید :");
-        if (budget == null || budget == "" || budget == "0" || isNaN(budget)) {
-            window.location.reload();
+    getBudgetAndShow() {
+
+        // get left orver money  and budget of localstorage
+        let lom = this.checkContentExistINlocalStorage("left-over-money"),
+            bu = this.checkContentExistINlocalStorage("budget");
+
+
+        if (lom[0] < bu[0] && lom[0] > 0) {
+
+            //create span for show budget
+            let span = document.createElement("span");
+            span.innerHTML = this.addComma(bu[0]);
+            span.classList = "span";
+
+            // budget
+            let totalMoney = document.querySelector(".totoal-money");
+            totalMoney.firstElementChild.appendChild(span);
+
+            let leftOverMoney = document.querySelector(".left-over-money");
+            //create span for show left Over Money
+            let spanLOM = document.createElement("span");
+
+            spanLOM.classList = "spanLOM"
+
+            spanLOM.innerHTML = this.addComma(lom[0]);
+
+            leftOverMoney.firstElementChild.appendChild(spanLOM);
+
+            // load factors of local storage 
+            let factors = this.checkContentExistINlocalStorage("factors");
+            // show factors in page 
+            factors.forEach(element => {
+                //crete element li for show spend item
+                const li = document.createElement("li");
+                li.innerHTML = element;
+                const ul = document.querySelector(".spending");
+                ul.appendChild(li);
+            });
+
+
+        } else {
+            // empty factors in local storage
+            let factors = this.checkContentExistINlocalStorage("factors");
+            factors = []
+            localStorage.setItem("factors", JSON.stringify(factors));
+
+            if (lom[0] == 0) {
+                alert(" بودجه هفتگی شما به اتمام رسیده است ) :")
+            }
+            // get all the money OF user 
+            let budget = prompt("لطفا مقدار بودجه ی خود را به تومان وارد کنید :");
+            if (budget == null || budget == "" || budget == "0" || isNaN(budget)) {
+                window.location.reload();
+            }
+
+
+            // sperete width comma 
+            let money = this.addComma(budget);
+
+            //create span for show budget
+            let span = document.createElement("span");
+            span.innerHTML = money;
+            span.classList = "span";
+
+            // budget
+            let totalMoney = document.querySelector(".totoal-money");
+            totalMoney.firstElementChild.appendChild(span);
+
+            let leftOverMoney = document.querySelector(".left-over-money");
+            //create span for show left Over Money
+            let spanLOM = document.createElement("span");
+
+            spanLOM.classList = "spanLOM"
+
+            spanLOM.innerHTML = money;
+
+            leftOverMoney.firstElementChild.appendChild(spanLOM);
+
+            // save budget to local storage
+            let budge = this.checkContentExistINlocalStorage("budget");
+            budge.pop();
+            budge.push(money.replace(/,/g, ""));
+            localStorage.setItem("budget", JSON.stringify(budge));
+            localStorage.setItem("leftOverMoney", JSON.stringify(budge));
+
         }
-
-
-        // sperete width comma 
-        let money = this.addComma(budget);
-
-        //create span for show budget
-        let span = document.createElement("span");
-        span.innerHTML = money;
-        span.classList = "span";
-
-        // budget
-        let totalMoney = document.querySelector(".totoal-money");
-        totalMoney.firstElementChild.appendChild(span);
-
-        let leftOverMoney = document.querySelector(".left-over-money");
-        //create span for show left Over Money
-        let spanLOM = document.createElement("span");
-
-        spanLOM.classList = "spanLOM"
-
-        spanLOM.innerHTML = money;
-
-        leftOverMoney.firstElementChild.appendChild(spanLOM);
-
-        // save budget to local storage
-        let budge = this.checkContentExistINlocalStorage("budget");
-        budge.pop();
-        budge.push(money.replace(/,/g, ""));
-        localStorage.setItem("budget", JSON.stringify(budge));
-        localStorage.setItem("leftOverMoney", JSON.stringify(budge));
-
     }
 
     // spreted number in input price
@@ -178,8 +227,8 @@ class GetInformations {
             contentLocalstorage = localStorage.getItem("budget");
         } else if (assessment == "left-over-money") {
             contentLocalstorage = localStorage.getItem("leftOverMoney");
-        } else if (assessment == "prices") {
-            contentLocalstorage = localStorage.getItem("prices");
+        } else if (assessment == "factors") {
+            contentLocalstorage = localStorage.getItem("factors");
         }
 
         // check value in local storege
